13f6606f787771b24ece91e7c58f7374a1cacc0a
Alokananda Y <plantsfortrees@gmail.com>
2025-03-09 07:10:18 +0000

    Learn HTML and CSS: The Ultimate Beginner's Guide

--- JavaScript-Roadmap/DOM_and_Events/example_1/app.js
+++
@@ -1,7 +0,0 @@
-const anchorElement = document.querySelector('a');
-const paraElement = document.querySelector('p');
-let paraTextContent = paraElement.textContent;
-
-anchorElement.addEventListener('click', () => {
-    paraElement.textContent = `${paraTextContent} Check the new tab!`;
-})
--- README.md
+++
@@ -1,4 +0,0 @@
-# Learn To Code with  
-Real Life Coding Projects
-
-Become a proficient front-end or full-stack coder with our carefully curated coding challenges. Improve your skills and land your dream developer job.
