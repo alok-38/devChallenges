84ffbf30f09a1ad596669efbb14791aa9679dc22
plantsfortrees@gmail.com <Alokananda Y>
2025-08-19 01:08:51 +0000

    Understanding the CSS Box Model

--- essential_CSS/script.js
+++
@@ -1,4 +0,0 @@
-const inputEl = document.querySelector('input');
-const inputElWidth = getComputedStyle(inputEl).width;
-inputEl.style.width = "50%"; // ✅ set width properly
-console.log(inputElWidth);     // logs original width
