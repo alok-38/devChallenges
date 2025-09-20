0c36b521c87ac141cb64328d11a73bf294355d27
plantsfortrees@gmail.com <Alokananda Y>
2025-09-20 12:58:34 +0000

    Improving solution

--- flip-the-coin-master/app.js
+++ flip-the-coin-master/app.js
@@ -1,1 +1,26 @@
 const randomButton = document.querySelector('button');
+const coinImage = document.querySelector('img');
+const headsOrTails = document.querySelector('h2');
+
+function flipCoin() {
+    // Add flip class to animate
+    coinImage.classList.add('flip');
+
+    // Wait 300ms before switching the image (halfway through the animation)
+    setTimeout(() => {
+        const isHeads = Math.random() < 0.5;
+
+        if (isHeads) {
+            coinImage.src = 'resources/tails.svg';
+            headsOrTails.textContent = "Tails";
+        } else {
+            coinImage.src = 'resources/heads.svg';
+            headsOrTails.textContent = "Heads";
+        }
+
+        // Remove flip class to allow re-flipping
+        coinImage.classList.remove('flip');
+    }, 300);
+}
+
+randomButton.addEventListener('click', flipCoin);
