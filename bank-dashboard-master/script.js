41c6808754ae03af6c959e18a091fb1adc82f4c3
plantsfortrees@gmail.com <Alokananda Y>
2025-12-21 04:48:25 +0000

    Submitting solution

--- bank-dashboard-master/script.js
+++
@@ -1,8 +0,0 @@
-const avatarEl = document.querySelector('.top-nav__avatar');
-const userName = document.querySelector('.top-nav__name');
-
-const displayUsername = () => {
-    userName.textContent = "Alok";
-}
-
-avatarEl.addEventListener('click', displayUsername);
